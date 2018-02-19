421246adfdd5df05a038c3c3d432e9e3b01e479b
omigibson <oumie.gibba@gmail.com>
2018-02-19 16:07:11 +0100

    Fix problems with buttons not working  after new div been added for styling

--- main.js
+++ main.js
@@ -18,45 +18,45 @@ addButton.addEventListener("click", function () {
 	
 	var todos = document.getElementById("todos");
 	todos.appendChild(li);
+	var liButtons = document.createElement("div");
+	liButtons.className = "liButtons";
+	li.appendChild(liButtons);
 	
 	var doneButton = document.createElement("button");
 	doneButton.className = "doneButton btn btn-outline-success btn-sm";
 	doneButton.innerHTML = "Klar";
 	doneButton.addEventListener("click", function(){
-		var listItem = this.parentNode;
-		listItem.removeChild(this);
+		var liButtons = this.parentNode;
+		liButtons.removeChild(this);
+		var listItem = liButtons.parentNode;
 		var done = document.getElementById("done");
 		done.appendChild(listItem);
 	});
-	li.appendChild(doneButton);
+	liButtons.appendChild(doneButton);
 	
 	var deleteButton = document.createElement("button");
 	deleteButton.className = "deleteButton btn btn-outline-danger btn-sm";
 	deleteButton.innerHTML = "Radera";
 	deleteButton.addEventListener("click", function(){
-	var listItem = this.parentNode;
+	var listItem = this.parentNode.parentNode;
 	listItem.parentNode.removeChild(listItem);
 });
-	li.appendChild(deleteButton);
+	liButtons.appendChild(deleteButton);
 
 });
 
-/*
-Klarmarkera-knapp. Denna kod behövs för de todos som är hårdkodade in i index.html. 
-1. Alla klarmarkera-knappar hämtas via klass och sparas som en NodeList i variabeln doneButtons.
-2. NodeListen loopas igenom och varje knapp får en EventListener som lyssnar efter klick.
-3. Inuti funktionen hämtas den klickade knappens förälder och sparas i listItem.
-4. Klarmarkera-knappen tas bort från listItem med removeChild och this.
-5. Klar-listan hämtas via id.
-5. listItem läggs till i klarlistan.
-*/
+// Klarmarkera-knapp för enskild todo. Denna kod behövs för de todos som är hårdkodade in i index.html. 
 
+// Alla klarmarkera-knappar hämtas via klass och sparas som en NodeList i variabeln doneButtons. NodeListen loopas igenom och varje knapp får en EventListener som lyssnar efter klick.
 var doneButtons = document.getElementsByClassName("doneButton");
-
 for(i = 0; i < doneButtons.length; i++){
 	doneButtons[i].addEventListener("click", function(){
-		var listItem = this.parentNode;
-		listItem.removeChild(this);
+		/* Den klickade knappens förälder hämtas och sparas sparas i liButtons. Med hjälp av removeChild kan Klarmarkera-knappen tas bort från liButtons. Den behövs inte längre när uppgiften är avklarad.*/
+		var liButtons = this.parentNode;
+		liButtons.removeChild(this);
+		
+		// liButtons förälder, ett <li> element, hämtas och sparas i listItem. Klar-listan hämtas via id och listItem läggs till där med appendChild.
+		var listItem = liButtons.parentNode;
 		var done = document.getElementById("done");
 		done.appendChild(listItem);
 	});
@@ -74,7 +74,7 @@ var deleteButtons = document.getElementsByClassName("deleteButton");
 
 for(i = 0; i < deleteButtons.length; i++){
 deleteButtons[i].addEventListener("click", function(){
-	var listItem = this.parentNode;
+	var listItem = this.parentNode.parentNode;
 	listItem.parentNode.removeChild(listItem);
 });
 }
